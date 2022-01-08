38c6fb8fd685a99c48d8b20a424c2b5cde09a89e
OULD AMARA Amine <ouldamaraamine@gmail.com>
2022-01-08 23:30:43 +0100

    about to modularize code

--- basicFim.sh
+++ basicFim.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+figlet A basic FIM
+#User input 
 echo -ne "would you like to\n	1) Create a new baseline\nOr\n	2) Proceed with the previously recorded one\n1 | 2 ? "
 read ans
-echo "Creating new baseline"
  
+
+#function that calculates the filehash for the specified file directory in function call argument 
 function calculate_file_hash(){
 	filehash=$(sha256sum $1 | cut -d ' ' -f 1)
 	filepath=$1
-	x=$filepath"|"$filehash  
-	#echo $x
+	echo $filepath"|"$filehash  
 	echo $x >> baseline.txt
 }
+#store filepath - filehash in a key-value dictionnary
 
 
 #verify if input is either 1 or 2 ----------------------------------------------
+
+
+
+
+
+#--------------------------------------- Create new baseline -------------------------------------------
 if [ "$ans" = "1" ];then
+	echo "Creating new baseline"
+
 #calculate hash from the target files and store them in a baseline.txt file 
 	monitoring_dir=$(pwd)
 	#delete baseline file if already exists
 	if [ -f "baseline.txt" ]; then
-		echo "exists"
+		echo -e "baseline already exists !\ndeleting old baseline...\ncreating new baseline..."
 		rm baseline.txt
 		>baseline.txt
 	fi
@@ -27,30 +38,51 @@ if [ "$ans" = "1" ];then
 		calculate_file_hash "$entry"
 	done
 	echo "Baseline created"
-
-	#collect all files in the target 
-	declare -A test_var
+	
+	declare -A path_hash_dict
 	cat baseline.txt | while read line 
 	do
 		path=$( echo "$line" | cut -d '|' -f1 )
 		hash=$( echo "$line" | cut -d '|' -f2-)
-		test_var[$path]=$hash
+		path_hash_dict[$path]=$hash
 	done
-	# echo "${test_var[/home/oaamine/Projects/file_integrity_monitor/basicFim.sh]}"
+	
+	for key in "${!path_hash_dict[@]}"; do
+		if [ ! -f "$key" ]; then
+			echo -e "A file has been removed !\n file name : $key"
+			# echo "$key ${path_and_hash[$key]}"
+		fi
+	done
+	# for entry in "$monitoring_dir"/*
+	# do
+	# 	#hash=sha256sum $entry
+	# 	if [ ! -v path_hash_dict[$entry] ]; then
+    # 	echo -e "A new file has been found !\n ${ls -l $entry}"
+	# 	fi
+	# done
 
 
-	for entry in "$monitoring_dir"/*
+
+#------------------------------------ Proceed with the previously recorded baseline ----------------------------
+else
+	declare -A path_hash_dict
+	Lines=$(cat baseline.txt)
+	# echo "$Lines" 
+	for line in $Lines 
 	do
-		hash = sha256sum $entry
-		if [ ! -v test_var[$entry] ]; then
-    	echo "key2 does not exists in a dictionary"
+		path=$( echo "$line" | cut -d '|' -f1 )
+		hash=$( echo "$line" | cut -d '|' -f2-)
+		path_hash_dict[$path]=$hash
+	done
+	#check if a file has been removed 
+	for key in "${!path_hash_dict[@]}"; do
+		if [ ! -f "$key" ]; then
+			echo -e "A file has been removed !\n file name : $key"
 		fi
 	done
 
-else
 
 
-	
- 	echo "no"
+
 fi
 
