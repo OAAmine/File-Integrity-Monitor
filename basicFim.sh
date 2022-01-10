ce7a2c5072614ff80ae6ae6c30b70f075f2f1a88
OAAmine <ouldamaraamine@gmail.com>
2022-01-10 15:24:14 +0100

    working basic FIM...adding some extra features

--- basicFim.sh
+++ basicFim.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+
+#create logs file if it doesn't exit 
+if [ ! -f ".baseline.txt" ]; then
+	touch .logs.txt
+fi
+
+
+
+#Must run script as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+#some decoration (figlet has to be installed on your system)
 figlet A basic FIM
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+ORANGE='\033[0;33m'
+
 #User input 
-echo -ne "would you like to\n	1) Create a new baseline\nOr\n	2) Proceed with the previously recorded one\n1 | 2 ? "
+echo -ne "would you like to\n	1) Create a new .baseline\nOr\n	2) Proceed with the previously recorded one\n1 | 2 ? "
 read ans
  
 
@@ -9,11 +30,9 @@ read ans
 function calculate_file_hash(){
 	filehash=$(sha256sum $1 | cut -d ' ' -f 1)
 	filepath=$1
-	echo $filepath"|"$filehash  
-	echo $x >> baseline.txt
+	path_and_hash=$filepath"|"$filehash
+	echo $path_and_hash
 }
-#store filepath - filehash in a key-value dictionnary
-
 
 #verify if input is either 1 or 2 ----------------------------------------------
 
@@ -21,68 +40,83 @@ function calculate_file_hash(){
 
 
 
-#--------------------------------------- Create new baseline -------------------------------------------
+#--------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
+#--------------------------------------- Create new .baseline -------------------------------------------
 if [ "$ans" = "1" ];then
-	echo "Creating new baseline"
+	echo "Creating new .baseline"
 
-#calculate hash from the target files and store them in a baseline.txt file 
+	#calculate hash from the target files and store them in a .baseline.txt file 
 	monitoring_dir=$(pwd)
-	#delete baseline file if already exists
-	if [ -f "baseline.txt" ]; then
-		echo -e "baseline already exists !\ndeleting old baseline...\ncreating new baseline..."
-		rm baseline.txt
-		>baseline.txt
+	
+	
+	#delete .baseline file if already exists
+	if [ -f ".baseline.txt" ]; then
+		echo -e ".baseline already exists !\ndeleting old .baseline...\ncreating new .baseline..."
+		rm .baseline.txt
+		>.baseline.txt
 	fi
+	
+	
+	#filling in the .baseline.txt file with filepath|filehash pairs
 	for entry in "$monitoring_dir"/*
 	do
-		calculate_file_hash "$entry"
+		res=$(calculate_file_hash "$entry")
+		echo $res >> .baseline.txt
 	done
-	echo "Baseline created"
+	sudo chmod 777 .baseline.txt 
+	echo ".baseline created"
 	
-	declare -A path_hash_dict
-	cat baseline.txt | while read line 
-	do
-		path=$( echo "$line" | cut -d '|' -f1 )
-		hash=$( echo "$line" | cut -d '|' -f2-)
-		path_hash_dict[$path]=$hash
-	done
-	
-	for key in "${!path_hash_dict[@]}"; do
-		if [ ! -f "$key" ]; then
-			echo -e "A file has been removed !\n file name : $key"
-			# echo "$key ${path_and_hash[$key]}"
-		fi
-	done
-	# for entry in "$monitoring_dir"/*
-	# do
-	# 	#hash=sha256sum $entry
-	# 	if [ ! -v path_hash_dict[$entry] ]; then
-    # 	echo -e "A new file has been found !\n ${ls -l $entry}"
-	# 	fi
-	# done
 
 
 
-#------------------------------------ Proceed with the previously recorded baseline ----------------------------
+#--------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------# 
+#------------------------------------ Proceed with the previously recorded .baseline ----------------------------
 else
 	declare -A path_hash_dict
-	Lines=$(cat baseline.txt)
-	# echo "$Lines" 
-	for line in $Lines 
+	Lines=$(cat .baseline.txt)
+	monitoring_dir=$(pwd)
+
+
+	#creating a dictionary with filepath as key and filehash as value
+	lines=$(cat .baseline.txt)
+	echo -e "Start...\nMonitoring Files...\nYou will be notified of any changes here\nFor more details about changes made, see logs.txt file\nPress [CTRL+C] to stop monitoring."
+	for line in $lines 
 	do
 		path=$( echo "$line" | cut -d '|' -f1 )
 		hash=$( echo "$line" | cut -d '|' -f2-)
 		path_hash_dict[$path]=$hash
+	done 
+	while true
+	do
+		sleep 1
+		
+		#checking if a file has been deleted 
+		for key in "${!path_hash_dict[@]}"; do
+			if [ ! -f "$key" ]; then
+				echo -e "${RED}WARNING :${NC} a file has been ${ORANGE}REMOVED ! ${NC}\n${BLUE}FILE NAME :${NC} $key"
+			fi
+		done
+
+
+		for file in "$monitoring_dir"/*
+		do
+			hash=$(sha256sum $file | cut -d ' ' -f 1) s
+			if [ ! -v path_hash_dict[$file] ]; then
+				echo -e "${RED}WARNING :${NC} a file has been ${ORANGE}CREATED ! ${NC}\n${BLUE}FILE NAME :${NC} $key"
+			else
+				if [ "$hash" = "${path_hash_dict[$file]}" ]; then
+				   continue
+				else
+					echo -e "${RED}WARNING :${NC} a file has been ${ORANGE}CHANGED ! ${NC}\n${BLUE}FILE NAME :${NC} $key"
+				fi
+			fi
+		done
+
 	done
-	#check if a file has been removed 
-	for key in "${!path_hash_dict[@]}"; do
-		if [ ! -f "$key" ]; then
-			echo -e "A file has been removed !\n file name : $key"
-		fi
-	done
-
-
-
-
+	
 fi
 
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-declare -A test_var
-test_var[key1]=5
-test_var['key2']='value2'
-echo ${test_var[key1]}
-echo ${test_var['key2']}
